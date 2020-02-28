30a45cfb2055792d1705d558480983cb85126344
nikst <nikst90@gmail.com>
2020-02-28 14:54:57 +0200

    made separate js

---
+++ Classwork/02-27/app2.js
@@ -0,0 +1,10 @@
+let user = prompt('enter usermane');
+let pass = prompt("enter password");
+
+if (user == "admin" && pass == "master") {
+    alert("Hello");
+} else if (user!=="" && pass == user + 11) {
+    let res = prompt("Hello");
+} else {
+    alert = ("viso gero");
+}
