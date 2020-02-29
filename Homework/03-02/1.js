2ab00f4ab104420ab3d2e43aed872b1001d151c3
nikst <nikst90@gmail.com>
2020-02-29 22:38:00 +0200

    save 02/29

---
+++ Homework/03-02/1.js
@@ -0,0 +1,33 @@
+let three = "3";
+let a = b1 = 10; // 1st part
+let b = d = 2;
+let c = 15; // 2nd part
+let result = a1 = c+d; // result = 17
+let c1 = b1 - a1;
+let d1 = 7;
+let result_2 = d1 + c1
+let str = "Hello World"
+function nr_1() {
+    document.getElementById("result").innerHTML = three;
+}
+function Addition() {
+    document.write(a+b);
+}
+function Subtraction() {
+    document.write(a-b);
+}
+function Multiplication() {
+    alert(a*b);
+}
+function Division() {
+    alert(a/b);
+}
+function nr_3() {
+    alert(result);
+}
+function nr_4() {
+    alert(result_2)
+}
+function nr_5() {
+    document.write (str)
+}
