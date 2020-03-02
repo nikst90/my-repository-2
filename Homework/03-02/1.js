958a67751cc15536211a4594b33bb8d08c24167e
nikst <nikst90@gmail.com>
2020-03-02 14:57:15 +0200

    added hw

--- Homework/03-02/1.js
+++ Homework/03-02/1.js
@@ -3,31 +3,57 @@ let a = b1 = 10; // 1st part
 let b = d = 2;
 let c = 15; // 2nd part
 let result = a1 = c+d; // result = 17
-let c1 = b1 - a1;
+let c1 = a1 - b1;
 let d1 = 7;
-let result_2 = d1 + c1
-let str = "Hello World"
+let result_2 = d1 + c1;
+let str = "Hello World!";
+let str1 = "Hello";
+let str2 = "World!";
+let name = "Nikita";
+let age = 30;
+let test = " year "
+let test1;
+if (age > 1) {
+    test1 = " years "
+}
+else {
+    test1 = test
+}
+
 function nr_1() {
-    document.getElementById("result").innerHTML = three;
+    alert(three);
 }
 function Addition() {
-    document.write(a+b);
+    document.getElementById("result").innerHTML = a+b;
 }
 function Subtraction() {
-    document.write(a-b);
+    document.getElementById("result").innerHTML = a-b;
 }
 function Multiplication() {
-    alert(a*b);
+    document.getElementById("result").innerHTML = a*b;
 }
 function Division() {
-    alert(a/b);
+    document.getElementById("result").innerHTML = a/b;
 }
 function nr_3() {
-    alert(result);
+    document.getElementById("result").innerHTML = result;
 }
 function nr_4() {
-    alert(result_2)
+    document.getElementById("result").innerHTML = result_2;
 }
 function nr_5() {
-    document.write (str)
+    document.getElementById("result").innerHTML = str;
+}
+function nr_6() {
+    document.getElementById( "result").innerHTML = str1+" "+str2;
+}
+function nr_7() {
+    document.getElementById("result").innerHTML = "Hello " + name + "!";
+}
+function nr_8() {
+    document.getElementById("result").innerHTML = "I am " + age + test1 + "old!";
+}
+function nr_9() {
+    name1 = prompt("enter your name");
+    alert("Your name is " + name1);
 }
