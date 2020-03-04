024384773b41b4e7bba994798c4ebdb7543831ff
nikst <nikst90@gmail.com>
2020-03-04 15:16:27 +0200

    class work fin, do .. while and if

---
+++ Classwork/03-02/app.js
@@ -0,0 +1,23 @@
+let time
+
+do {
+    time = prompt('enter time');
+    isNaN(time)==true || time<0 || time >24
+}while(isNaN(time)==true || time<0 || time >24)
+
+ if (0<= time && time<= 3 || time == 24) {
+    alert('naktis');
+}
+ else if (4<=time && time<=11) {
+    alert("rytas");
+}
+ else if (12<=time && time<=17){
+     alert("diena");
+}
+ else if (18<=time && time <=24){
+     alert("vakaras")
+ }
+ else {
+    alert('Klaida');
+}
+
