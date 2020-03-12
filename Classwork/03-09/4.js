38a22a95901b7e41c652f8761c41f34549bb57d2
nikst <nikst90@gmail.com>
2020-03-12 12:55:04 +0200

    added homework

--- Classwork/03-09/4.js
+++ Classwork/03-09/4.js
@@ -1,27 +1,68 @@
+// Funkcija skirta "pakelti" pirma perduodamo string'o zenkla
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    return // Grazinamas rezultatas
+    string.charAt(0).toUpperCase() // pirmas perduodamo string'o zenklas "pakeliama"
+    + // sujungimas
+    string.slice(1); // gaunasmas string'as nuo pirmo zenklo iki galo
 }
 
-function toTitleCase(str,d='') {
-    let words = str.split(' ');
+// Funkcija tikrina ar perduota reiksme yra raide
+function isLetter(str) {
+    return str.toLowerCase() != str.toUpperCase();
+}
 
-	for (let i in words) {
-        words[i] = capitalizeFirstLetter(words[i]);
+// Funkcija konvertuoja perduodama string'a i "Title Case"
+// 1 parametras - pats string'as
+// 2 parametras - nurodoma palei ka isskaidyti nurodyta pirmai string'a, palei nutilejima yra tarpas
+function toTitleCase(str, d = ' ') {
+    // gauta stringa isskaidoma i dalis palei nurodyta skaidimo parametra
+    let parts = str.split(d);
+
+    // cikle prasukam kieviena isskaidyta dali
+    for (let i in parts) {
+        // sukuriame flag'a
+        // Issamiaiu apie flagus https://www.geeksforgeeks.org/use-of-flag-in-programming/
+        let add_first = false;
+
+        // patikriname ar pirmas zenklas pas mus yra raide
+        // jeigu ne tai issaugojama ja ateiciai
+        // ir istriname
+        if (isLetter(parts[i].charAt(0)) == false) {
+            add_first = parts[i].charAt(0);
+
+			parts[i] = parts[i].slice(1);
+        }
+
+        // pakeiciame pirma raide i Upper Case
+        parts[i] = capitalizeFirstLetter(parts[i]);
+
+        // jeigu is anksciau mes buvome istrine pirmaji zenkla pries raide
+        // pridedame ji atgal
+        if (add_first) {
+            parts[i] = add_first + parts[i];
+        }
     }
 
-	return words.join(' ');
+    // sulipdome musu rezultata panaudojant skirimo parametra ir graziname ji
+    return parts.join(d);
 
-	console.log(words);
+    // po return'o joks kodas nebebus vykdomas
+    console.log(parts);
 }
 
 let name = 'petras petraitis';
 
-name = toTitleCase(name);
+// name = toTitleCase(name);
 
-console.log(`Hello, ${name}`);
+// console.log(`Hello, ${name}`);
 
 let str = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod';
 
-str = toTitleCase(str, ',');
+let str_u = toTitleCase(str, ',');
 
 console.log(str);
+console.log(str_u);
+
+
+
+
---
+++ Homework/03-12/1.js
@@ -0,0 +1,38 @@
+//Parašykite funkciją, kuri grąžina skaičiaus kvadratą. Skaičius perduodamas kaip parametras.
+
+function square(squareid){
+    return squareid*squareid;
+    }
+square(5);
+
+    //Parašykite funkciją, kuri grąžina dviejų skaičių sumą. Skaičiai perduodami funkcijos parametrus.
+function sumNr(sumid,sumid2){
+    let sumid3=sumid+sumid2;
+    return  sumid3;
+}
+sumNr( 1,2)
+
+  //  Parašykite funkciją, kuri iš antro parametro atima pirmą ir padalija iš trečio.
+
+function solution(item1,item2,item3){
+    let sumid4=(item2-item1)/item3;
+    return  sumid4;
+}
+solution( 2,14,3);
+
+
+// Parašykite funkciją, kuri priima kaip parametrą skaičių nuo 1 iki 7, o grąžina savaitės dieną lietuvių kalba.
+
+function dayOfTheWeek (dayNr) {
+    let week = {
+        1: "pirmadienis",
+            2: "antradienis",
+            3: "treciadienis",
+            4: "ketvirtadienis",
+            5: "penktadienis",
+            6: "sestadienis",
+            7: "sekmadienis"
+    };
+    let dayNrCheck=week[dayNr];
+    return dayNrCheck;
+}
