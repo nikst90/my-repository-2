12c10c8ee0f81fd8d8158cc832b7637084ba1e9b
nikst <nikst90@gmail.com>
2020-03-16 09:07:34 +0200

    edited file

--- Homework/03-16/script.js
+++ Homework/03-16/script.js
@@ -8,6 +8,8 @@
 
 
 
+let inputCaps = document.querySelector("#inputCaps");
+
 
 document.getElementById("btn_1").addEventListener("click", function() {
     alert("Hello World!");
@@ -21,7 +23,11 @@ document.getElementById("btn_toLowCase").addEventListener("click", function() {
     document.getElementById("inputCaps").style.textTransform = "lowercase";
 });
 document.getElementById("btn_firstUpCase").addEventListener("click", function() {
-            document.getElementById("inputCaps").style.textTransform = "capitalize";
+            document.querySelector(inputCaps).addEventListener("click", function() {
+                inputCaps.value = capitalizeFirstLetter(inputCaps.value)
+
+
+
 });
 
 // document.getElementById("btn_firstLowCase").addEventListener("click", function() {
@@ -35,6 +41,7 @@ document.getElementById("btn_firstUpCase").addEventListener("click", function() 
 
 
 
+
 //
 //
 //
@@ -43,21 +50,23 @@ document.getElementById("btn_firstUpCase").addEventListener("click", function() 
 //     return string.charAt(0).toUpperCase() + string.slice(1);
 // }
 //
-// //First caps, after comma caps.
-// function lowercase1(string) {
-//     return string.charAt(0).toLowerCase() + string.slice(1);
+
+
+//First caps, after comma caps.
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+// function isLetter(val) {
+//     return val.toLowerCase() != val.toUpperCase();
 // }
-// // ​
-// // function isLetter(val) {
-// //     return val.toLowerCase() != val.toUpperCase();
-// // }
-// // ​
+//
 // function toTitleCase(str, d = ' ') {
 //     let parts = str.split(d);
-// ​
+//
 // 	for (let i in parts) {
 //         let special = [];
-// ​
+//
 // 		for (j = 0; j < parts[i].length; j++) {
 //             if (isLetter(parts[i].charAt(j)) === false) {
 //                 special.push(parts[i].charAt(j));
@@ -65,31 +74,23 @@ document.getElementById("btn_firstUpCase").addEventListener("click", function() 
 //                 break;
 //             }
 //         }
-// ​
+//
 //         parts[i] = parts[i].slice(special.length);
-// ​
+//
 //         parts[i] = capitalizeFirstLetter(parts[i]);
-// ​
+//
 // 		if (special.length > 0) {
 //             parts[i] = special.join('') + parts[i];
 //         }
 //     }
-// ​
+//
 // 	return parts.join(d);
 // }
-// ​
-// let name = 'petras petraitis';
-// ​
-// // name = toTitleCase(name);
-// ​
-// // console.log(`Hello, ${name}`);
-// ​
-// let str = 'Lorem ipsum dolor sit amet, .  . 1  consectetur adipisicing elit, sed do eiusmod';
-// ​
-// console.log(str);
-// ​
-// let str_u = toTitleCase(str, ',');
-// ​
-// // console.log(str);
-// console.log(str_u);
 //
+// let name = 'petras petraitis';
+//
+// // name = toTitleCase(name);
+//
+// // console.log(`Hello, ${name}`);
+//
+// let str = 'Lorem ipsum dolor sit amet, .  . 1  consectetur adipisicing elit, sed do eiusmod';
